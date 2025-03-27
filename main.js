16b974eaa1dc27ca37a60eb3fec30dee44666172
fwmc-dev <diegopaul483@gmail.com>
2025-03-27 22:37:54 +0000

    subo prueba terminada

---
+++ main.js
@@ -0,0 +1,12 @@
+const multiplicar = (x, y) => { return x * y };
+
+function calcularMultiplicacion(a, b) {
+    let resultado = multiplicar(a, b);
+
+    console.log("Resultado de la multiplicación: ", resultado);
+
+    //imprimir(resultado);
+}
+
+calcularMultiplicacion(3, 7);
+// Resultado de la multiplicación: 21
